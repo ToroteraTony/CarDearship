bf3dc8470682642ef137ad37d988da524271b272
Antonio <antoniomiljan@gmail.com>
2020-11-30 15:36:41 +0100

    Becic & Butigan update

--- src/frontend/assets/js/cars.js
+++ src/frontend/assets/js/cars.js
@@ -13,6 +13,18 @@ const INSERT_CAR = "http://localhost:8080/api/insertCar/";
 //  Needs {id} added
 const DELETE_CAR = "http://localhost:8080/api/deleteCar/";
 
+// IF THE USER TRIES TO ACCESS THE DETAIL PAGE WITHOUT THE SPECIFIED CAR ID, THEY WILL BE REDIRECTED BACK TO INDEX
+if(window.location.href == "http://localhost/frontend/detail.html"){
+    window.location.replace("http://localhost/frontend/index.html");
+}
+
+// THIS CALLS METHODS APPROPRIATE TO THE USERS LOCATION
+if (window.location.href.indexOf("index") > -1) {
+    getCars(GET_ALL_CARS);
+}else if(window.location.href.indexOf("detail") > -1){
+    detail();
+}
+
 
 async function getCars(url) {
 
@@ -48,41 +60,34 @@ async function getCars(url) {
         }
     }
 
-
-    console.log(data);
-    //show(data);
-    
-    if (window.location.href.indexOf("index") > -1) {
+    if(window.location.href.indexOf("index") > -1){
+        showAll(data);
+    }else if(window.location.href.indexOf("detail") > -1) {
         show(data);
-    }else if(window.location.href.indexOf("detail") > -1){
-        detail(data);
     }
 
 }
-//Use this to call the right site
-if (window.location.href.indexOf("index") > -1) {
-    getCars(GET_ALL_CARS);
-}else if(window.location.href.indexOf("detail") > -1){
-    //This is hard coded you will have to look at cookies or whatever kind of way you want to
-    getCars(GET_CAR + "1");
+
+
+// DISPLAYS DATA OF A SINGLE CAR DEPENDING ON THE ID RECIEVED FROM THE URL
+function detail(){
+    
+    const querty = window.location.search;
+    const urlParam = new URLSearchParams(querty);
+    const id = urlParam.get('car_id');
+    const url = GET_CAR + id;
+
+    getCars(url);
+
 }
 
 
-
-
-function show(data){
+// DISPLAYS THE DATA FOR ALL CARS
+function showAll(data){
 const container = document.getElementById("carParent");
-    //let data = getCars(GET_ALL_CARS);
-    console.log(data);
 
 for (let e of Object.keys(data)) {
     var value = data[e];
-    //console.log(value);
-    const card = document.createElement("div");
-    //card.classList = "card col-md-6 col-lg-3 col-sm-6 mx-3";
-
-    //console.log(value);
-    //console.log(e, value);
 
     const content = `
 
@@ -101,21 +106,48 @@ for (let e of Object.keys(data)) {
                   <li class="list-group-item">${value.success.car.color}</li>
                 </ul>
                 <div class="card-body">
-                  <a href="detail.html?id=${value.success.car.id}" class="card-link">Card link</a>
+                  <a href="detail.html?car_id=${value.success.car.car_id}" class="card-link">Details</a>
                 </div>
                 <div class="card-footer text-muted">
                   2 days ago
                 </div>
             </div>
-
     
     `;
-
 
     container.innerHTML += content;
 }
 }
 
-function detail(data){
-    //fasdkfaldf
-}
+// DISPLAYS THE DETAILS FOR A CAR
+function show(data){
+    const container = document.getElementById("detailParentDiv");
+    
+        var value = data[0];
+    
+        const content = `
+
+    
+                <div class="div-left">
+                <h2> ${value.success.car.brand} ${value.success.car.model} </h2>
+
+                <ul class="list-group list-group-flush paddington">
+                <li class="list-group-item paddington">${value.success.car.brand} ${value.success.car.model}</li>
+                <li class="list-group-item paddington">${value.success.car.car_year}</li>
+                <li class="list-group-item paddington">${value.success.car.color}</li>
+                </ul>
+                </div>
+
+                <div class="div-right">
+                <svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" height="auto" width="100%" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
+                      <rect width="100%" height="100%" fill="#868e96"></rect>
+                      <text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text>
+                </svg>
+                </div>
+                     
+
+        
+        `;
+    
+        container.innerHTML += content;
+    }
